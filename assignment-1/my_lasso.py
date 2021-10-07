7fcda6f0a895c139a019b4f54710ffce19269e28
Oliver Hare <oliver.parsons.hare@gmail.com>
2021-10-06 22:54:31 -0400

    Update my_lasso.py

--- assignment-1/my_lasso.py
+++ assignment-1/my_lasso.py
@@ -6,6 +6,11 @@ import lasso_utilities as lu
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
+ZERO = np.zeros((1,1))
+
+def npsum(x):
+    return
+
 class MyLasso:
 
     def __init__(self):
@@ -46,7 +51,8 @@ class MyLasso:
                 "x={self.n}x{self.p}, y={y.shape[0]}x{y.shape[0]}")
 
         self.x = lu.standardize(x)
-        self.y = y, self.initial_w, self.initial_b0 = w, b
+        self.y = y
+        self.initial_w, self.initial_b0 = w, b
 
 
     def precomputation(self):
@@ -56,11 +62,12 @@ class MyLasso:
         self.x_y_inners = np.squeeze(np.matmul(np.rot90(self.y, 1), self.x)) # 1 x p -> p
         self.one_x_inners = np.squeeze(np.matmul(np.ones((1, self.n)), self.x)) # 1 x p -> p
         self.x_inners = np.matmul(np.rot90(np.flipud(self.x), 3), self.x) # p x p
-        self.null_dev = lu.deviance_func(0, self.y, 0, self.y_mean)
+        self.null_dev = lu.deviance_func(ZERO, self.y, ZERO, self.y_mean)
 
 
     def calc_gammas(self, lam) -> np.ndarray:
-        return np.apply_along_axis(lu.gamma_calc, 0, self.x_norms_sqrd, lam=lam) # p
+        gamma_func = lambda norm: (self.n * lam) / norm
+        return np.apply_along_axis(gamma_func, 0, self.x_norms_sqrd) # p
 
 
     def gen_lambdas(self, nlam: int) -> np.ndarray:
@@ -80,12 +87,13 @@ class MyLasso:
 
     def calc_new_values(self, weights: np.ndarray, b0: float,
             gammas: np.ndarray, ind: int) -> Tuple[float, float]:
-        new_b0 = self.y_mean - sum(np.multiply(self.initial_weights, self.x_means))
+
+        new_b0 = self.y_mean - np.multiply(weights, self.x_means).sum(dtype=np.int64)
 
         gamma = gammas[ind]
         z_x_inner = lu.calc_z_x_inner(
-            ind, self.x_y_inners, b0,
-            self.one_x_inners, weights,
+            ind, self.x_y_inners[ind], b0,
+            self.one_x_inners[ind], weights,
             self.x_inners[:, ind])
 
         b_hat = z_x_inner / self.x_norms_sqrd[ind]
@@ -123,12 +131,12 @@ class MyLasso:
 
 
     def fit(self, x: np.ndarray, y: np.ndarray, w: np.ndarray,
-            b0: float, thresh: float, iters: int=1000,
+            b0: float, thresh: float=1e-4, iters: int=1000,
             lams: np.ndarray=None, nlam: int=20, warm=True):
         self.initialize(x, y, w, b0)
         self.precomputation()
 
-        if lams == None:
+        if lams is None:
             lams = self.calc_lambdas(nlam)
 
         init_w = w
@@ -140,12 +148,12 @@ class MyLasso:
             model_df = np.count_nonzero(model_w)
 
             if model_score < self.final_score:
-                self.finale_score = model_score
+                self.final_score = model_score
                 self.final_w = model_w
                 self.final_b0 = model_b0
 
             model_entry = {"score": model_score, "dev": model_dev, "%%dev": model_pde,
-                           "df": model_df, "lambda": model_entry, "w": model_w, "b0": model_b0}
+                           "df": model_df, "lambda": lam, "w": model_w, "b0": model_b0}
             for key, value in model_entry.items():
                 self.results[key].append(value)
 
@@ -154,11 +162,24 @@ class MyLasso:
                 init_b0 = model_b0
 
 
-    def get_results(self, print=True):
-        if print:
+    def get_results(self, print_it=True):
+        if print_it:
             print("MyLasso Results")
-            print("Best Score: {self.final_score}")
+            print(f"Best Score: {self.final_score}")
             print_cols = ["score", "dev", "%%dev", "df", "lambda"]
-            print(pd.DataFrame.from_dict(self.results)[print_cols])
+            print_df = pd.DataFrame.from_dict(self.results)[print_cols]
+            betas = {}
+            for w in self.results["w"]:
+                for i in range(w.shape[0]):
+                    key = f"x{i}"
+                    val = w[i, 0]
+                    if key in betas:
+                        betas[key].append(val)
+                    else:
+                        betas[key] = [val]
+
+            betas_df = pd.DataFrame(data=betas)
+            print(print_df.join(betas_df))
+
 
         return self.final_score, self.final_w, self.final_b0, self.results
