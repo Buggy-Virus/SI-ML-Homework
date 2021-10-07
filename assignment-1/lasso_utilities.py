f10efe3a42685baf53e897b56eb0d2d3a9c8d750
Oliver Hare <oliver.parsons.hare@gmail.com>
2021-10-06 22:54:46 -0400

    Update lasso_utilities.py

--- assignment-1/lasso_utilities.py
+++ assignment-1/lasso_utilities.py
@@ -1,32 +1,37 @@
 import numpy as np
 
 def norm_squared(x: np.ndarray) -> float:
-    return np.sum(x**2)
+    return np.power(x, 2).sum(dtype=np.int64)
 
 
-def gamma_calc(n: int, lam: float, x_norm: float) -> float:
+def gamma_calc(x_norm: float, n: int, lam: float) -> float:
     return (n * lam) / x_norm
 
 
 def standardize(x: np.ndarray) -> np.ndarray:
-    return x.apply_along_axis(lambda col: (col - np.mean(col)) / np.std(col), 0, x)
+    return np.apply_along_axis(lambda col: (col - np.mean(col)) / np.std(col), 0, x)
 
 
 def soft_threshold(b: float, gamma: float) -> float:
-    return np.sign(b) * max((abs(b) - gamma), 0)
+    return np.sign(b) * np.clip((np.abs(b) - gamma), 0, None)
 
 
 def calc_z_x_inner(
-        ind: int, y_x_inner: float, intercept: float, one_x_inner: float,
-        weights: np.ndarray, x_inners: np.ndarray) -> float:
+    ind: int,
+    y_x_inner: float,
+    intercept: float,
+    one_x_inner: float,
+    weights: np.ndarray,
+    x_inners: np.ndarray
+) -> float:
     weights_copy = np.squeeze(np.copy(weights))
     weights_copy[ind] = 0
     return y_x_inner - intercept * one_x_inner - sum(np.multiply(weights_copy, x_inners))
 
 
 def deviance_func(x: np.ndarray, y: np.ndarray, w: np.ndarray, b: np.ndarray) -> float:
-    return np.sum(((y - b) - np.matmul(x, w))**2)
+    return (((y - b) - np.matmul(x, w))**2).sum(dtype=np.int64)
 
 
 def score_func(x: np.ndarray, y: np.ndarray, w: np.ndarray, b: np.ndarray, lam: float) -> float:
-    return (0.5 / y.shape[0]) * deviance_func(x, y, w, b) + lam * np.sum(np.absolute(b))
+    return (0.5 / y.shape[0]) * deviance_func(x, y, w, b) + lam * np.absolute(b).sum(dtype=np.int64)
